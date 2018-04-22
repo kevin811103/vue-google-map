e8d66920f312e13fe8530cf3f9dab1a4239b811c
chiakai.chang <chiakai.chang@leediancn.com>
2018-04-22 20:04:28 +0800

    -test

---
+++ src/components/Count.vue
@@ -0,0 +1,43 @@
+
+<template>
+  <div class="container">
+    <h2 style="color: red;">count: {{ count }}</h2>
+<h232432</h2>
+    <!-- 1. 增加 input 可以設定數值 model 綁定 num -->
+    Set Number: 
+    <input type='number' v-model='num' style="width: 50px;"><br/>
+    
+    <!-- 3. 雙向綁定 num 到按鈕上顯示-->
+    <!-- 傳 num 數值給 action -->
+    <button @click="actionIncrease(num)">+{{ num }}</button>
+    <button @click="actionDecrease(num)">-{{ num }}</button>
+    
+    <!-- 4. 新增歸零按鈕 -->
+    <button @click="actionCountReset">歸零</button>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  data () {
+    return {
+      // 2. model num 預設值為: 1
+      num: 1
+    }
+  },
+  computed: mapGetters({
+    count: 'getCount',
+  }),
+  methods: {
+    ...mapActions([
+      'actionIncrease',
+      'actionDecrease',
+
+      // 5. 引入歸零 action
+      'actionCountReset'
+    ])
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+        <h2 style="color: red;">count: {{ count }}</h2>
+<router-link to="/count">Go to count</router-link>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+    computed: mapGetters({
+    count: 'getCount',
+  }),
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
